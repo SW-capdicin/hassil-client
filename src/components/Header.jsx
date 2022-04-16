9224892f04ff53a4f2c64c172bc52f349442da07
yongjin <dydwls0669@gmail.com>
2022-04-16 13:22:49 +0900

    chore: header profile icon 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
 import logo from '@/img/logo.png';
+import { CgProfile } from 'react-icons/cg';
 
 const Header = () => {
   return (
     <Container>
-      <LogoImg></LogoImg>
+      <LogoImg />
+      {false && <CgProfile size={30} />}
     </Container>
   );
 };
 
 const Container = styled.header`
   width: 100%;
+  height: 5rem;
   display: flex;
   flex-direction: row;
-  justify-content: center;
+  align-items: center;
+  justify-content: space-around;
   margin-bottom: 2rem;
 `;
 
 const LogoImg = styled.div`
   background-image: url(${logo});
   background-repeat: no-repeat;
-  background-size: contain;
   background-position: center;
-  width: 20rem;
-  height: 6rem;
+  width: 10rem;
+  height: 3rem;
 `;
 
 export default Header;
