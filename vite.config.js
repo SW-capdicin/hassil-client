2a861ae854a7642f2b0ec98ea4ee23d019f0b46a
yongjin <dydwls0669@gmail.com>
2022-05-01 16:23:48 +0900

    Add api setting

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    proxy: {
+      '/api': { target: 'http://localhost:8080', changeOrigin: true },
+    },
+  },
 });
