d093caf829672c70f488f67e31023afe76b328fd
coderclcl <gattenmaster@gmail.com>
2022-05-05 19:00:06 +0900

    do NOT merge : cors 에러 test

--- src/api/user.js
+++ src/api/user.js
@@ -1,16 +1,21 @@
 import axios from 'axios';
 
+axios.defaults.withCredentials = true;
+
 export const getUserInfo = async () => {
-  const response = await axios.get('/api/users');
+  const response = await axios.get('http://localhost:8080/api/users');
   return response.data;
 };
 
 export const patchUserInfo = async (userInfo) => {
-  const response = await axios.patch('/api/users', userInfo);
+  const response = await axios.post(
+    'http://localhost:8080/api/users',
+    userInfo,
+  );
   return response.status;
 };
 
 export const logout = async () => {
-  const response = await axios.post('/api/users/logout');
+  const response = await axios.post('http://localhost:8080/api/users/logout');
   return response.status;
 };
