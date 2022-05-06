19925f8913032d2f384866eea5a854b102ad4617
유주현 <gus6359@gmail.com>
2022-05-07 05:43:47 +0900

    fix: remove header bottom margin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -61,7 +61,6 @@ const Container = styled.header`
   align-items: center;
   justify-content: ${(props) =>
     props.showUserIcon ? 'space-between' : 'center'};
-  margin-bottom: 2rem;
 `;
 
 const LogoImg = styled.div`
