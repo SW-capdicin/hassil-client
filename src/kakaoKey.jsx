df13fb8096dc7305ec49252e31e3c8f31f52e398
yongjin <dydwls0669@gmail.com>
2022-05-13 23:18:17 +0900

    Delete unneccessary file

--- src/kakaoKey.jsx
+++
@@ -1,4 +0,0 @@
-const { VITE_APP_KAKAO_MAP_KEY_API } = import.meta.env;
-const script_element = document.createElement('script');
-script_element.setAttribute('src', VITE_APP_KAKAO_MAP_KEY_API);
-document.querySelector('body').appendChild(script_element);
