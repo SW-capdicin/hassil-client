fc21ef1243a3f2e7109de577729782ff2cb705a3
yongjin <dydwls0669@gmail.com>
2022-05-22 23:01:10 +0900

    Make Cafe Main Page

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export { default as Header } from './Header';
 export { default as Input } from './Input';
 export { default as PublicRoute } from './PublicRoute';
 export { default as PrivateRoute } from './PrivateRoute';
+export { default as UserHome } from './UserHome';
+export { default as CafeHome } from './CafeHome';
