f74780cec5fac0acee84664e1a46f5a20c1c73bc
yongjin <dydwls0669@gmail.com>
2022-06-01 22:58:12 +0900

    스터디 생성 시 모달 추가

--- src/pages/StudyCreate.jsx
+++ src/pages/StudyCreate.jsx
@@ -1,7 +1,7 @@
 import 'react-datepicker/dist/react-datepicker.css';
 import React, { useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { PATH_HOME, TYPE_STUDY_CATEGORY } from '@/constants';
+import { PATH_HOME, TYPE_STUDY_CATEGORY, PATH_PAYMENT } from '@/constants';
 import styled from 'styled-components';
 import DatePicker from 'react-datepicker';
 import { ko } from 'date-fns/esm/locale';
@@ -13,7 +13,7 @@ import { useSelector } from 'react-redux';
 const CreateStudy = () => {
   const userState = useSelector((state) => state.user);
   const navigate = useNavigate();
-
+  const [openModal, setOpenModal] = useState(false);
   const [src, setFiles] = useState(null);
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
@@ -29,10 +29,10 @@ const CreateStudy = () => {
     minPerson: 1,
   });
   const { name, depositPerPerson, categoryId, operationTime } = inputs;
-  const selectList = [
-    // 나중에 category 조회로 불러올 것
-    ...TYPE_STUDY_CATEGORY,
-  ];
+  const selectList = [...TYPE_STUDY_CATEGORY];
+  const goPaymentPage = () => {
+    navigate(PATH_PAYMENT);
+  };
   const handleChange = (e) => {
     const nextInputs = {
       ...inputs,
@@ -40,9 +40,12 @@ const CreateStudy = () => {
     };
     setInputs(nextInputs);
   };
+  const toggleModal = () => {
+    setOpenModal((prevState) => !prevState);
+  };
   const handleSubmit = async () => {
     if (Number(userState.point) < Number(inputs.depositPerPerson)) {
-      alert('포인트가 부족합니다');
+      toggleModal();
       return;
     }
     if (
@@ -91,144 +94,212 @@ const CreateStudy = () => {
   };
 
   return (
-    <Container>
-      <SubContainer>
-        <InputContainer>
-          <Label>대표 이미지 등록</Label>
-        </InputContainer>
-        <SelectImg onClick={() => imgInput.current.click()}>
-          <Img src={src ? URL.createObjectURL(src) : emptyimg}></Img>
-        </SelectImg>
-        <Input
-          type="file"
-          accept="image/*"
-          style={{ display: 'none' }}
-          ref={imgInput}
-          onChange={onLoadFile}
-        />
-      </SubContainer>
-      <InputContainer>
-        <Label>제목</Label>
-        <Input
-          name="name"
-          value={name}
-          placeholder="STUDY WITH ME"
-          onChange={handleChange}
-        />
-      </InputContainer>
-      <InputContainer>
-        <Label>인당 보증금</Label>
-        <Input
-          name="depositPerPerson"
-          type="number"
-          defaultValue={depositPerPerson ? depositPerPerson : ''}
-          onChange={handleChange}
-          value={inputs.value}
-        />
-      </InputContainer>
-      <InputContainer>
-        <Label>시작 날짜</Label>
-        <SDatePickerContainer>
-          <SDatePicker
-            selected={startDate}
-            onChange={(date) => setStartDate(date)}
-            selectsStart
-            startDate={startDate}
-            endDate={endDate}
-            locale={ko}
-            minDate={new Date()}
-            dateFormat="yyyy년 MM월 dd일"
+    <>
+      <Container>
+        <SubContainer>
+          <InputContainer>
+            <Label>대표 이미지 등록</Label>
+          </InputContainer>
+          <SelectImg onClick={() => imgInput.current.click()}>
+            <Img src={src ? URL.createObjectURL(src) : emptyimg}></Img>
+          </SelectImg>
+          <Input
+            type="file"
+            accept="image/*"
+            style={{ display: 'none' }}
+            ref={imgInput}
+            onChange={onLoadFile}
           />
-        </SDatePickerContainer>
-      </InputContainer>
-      <InputContainer>
-        <Label>종료 날짜</Label>
-        <SDatePickerContainer>
-          <SDatePicker
-            selected={endDate}
-            onChange={(date) => setEndDate(date)}
-            selectsEnd
-            startDate={startDate}
-            endDate={endDate}
-            minDate={startDate}
-            locale={ko}
-            dateFormat="yyyy년 MM월 dd일"
-          />
-        </SDatePickerContainer>
-      </InputContainer>
-      <InputContainer>
-        <Label>분야</Label>
-        <Select name="categoryId" value={categoryId} onChange={handleChange}>
-          {selectList.map((item) => (
-            <option key={item.name} value={item.id}>
-              {item.name}
-            </option>
-          ))}
-        </Select>
-      </InputContainer>
-      <InputContainer>
-        <Label>운영시간</Label>
-        <Input
-          name="operationTime"
-          value={operationTime}
-          onChange={handleChange}
-          placeholder="오전 11:00~12:00"
-        />
-      </InputContainer>
-      <InputContainer>
-        <Label>최소 인원</Label>
-        <Input
-          name="minPerson"
-          type="number"
-          onChange={handleChange}
-          value={inputs.minPerson}
-        />
-      </InputContainer>
-      <InputContainer>
-        <Label>최대 인원</Label>
-        <Input
-          name="maxPerson"
-          type="number"
-          onChange={handleChange}
-          value={inputs.maxPerson}
-        />
-      </InputContainer>
-      <InputContainer>
-        <Label>결석 벌금</Label>
-        <Input
-          name="absentFee"
-          type="number"
-          onChange={handleChange}
-          value={inputs.absentFee}
-        />
-      </InputContainer>
-      <InputContainer>
-        <Label>지각 벌금</Label>
-        <Input
-          name="lateFee"
-          type="number"
-          onChange={handleChange}
-          value={inputs.lateFee}
-        />
-      </InputContainer>
-      <SubContainer>
+        </SubContainer>
         <InputContainer>
-          <Label>상세 정보</Label>
+          <Label>제목</Label>
+          <Input
+            name="name"
+            value={name}
+            placeholder="STUDY WITH ME"
+            onChange={handleChange}
+          />
         </InputContainer>
-        <TextArea
-          name="info"
-          onChange={handleChange}
-          placeholder="상세 정보를 입력해주세요"
-        />
-      </SubContainer>
-      <FixedDiv>
-        <CreateBtn onClick={handleSubmit}>
-          <BtnText>스터디 생성하기</BtnText>
-        </CreateBtn>
-      </FixedDiv>
-    </Container>
+        <InputContainer>
+          <Label>인당 보증금</Label>
+          <Input
+            name="depositPerPerson"
+            type="number"
+            defaultValue={depositPerPerson ? depositPerPerson : ''}
+            onChange={handleChange}
+            value={inputs.value}
+          />
+        </InputContainer>
+        <InputContainer>
+          <Label>시작 날짜</Label>
+          <SDatePickerContainer>
+            <SDatePicker
+              selected={startDate}
+              onChange={(date) => setStartDate(date)}
+              selectsStart
+              startDate={startDate}
+              endDate={endDate}
+              locale={ko}
+              minDate={new Date()}
+              dateFormat="yyyy년 MM월 dd일"
+            />
+          </SDatePickerContainer>
+        </InputContainer>
+        <InputContainer>
+          <Label>종료 날짜</Label>
+          <SDatePickerContainer>
+            <SDatePicker
+              selected={endDate}
+              onChange={(date) => setEndDate(date)}
+              selectsEnd
+              startDate={startDate}
+              endDate={endDate}
+              minDate={startDate}
+              locale={ko}
+              dateFormat="yyyy년 MM월 dd일"
+            />
+          </SDatePickerContainer>
+        </InputContainer>
+        <InputContainer>
+          <Label>분야</Label>
+          <Select name="categoryId" value={categoryId} onChange={handleChange}>
+            {selectList.map((item) => (
+              <option key={item.name} value={item.id}>
+                {item.name}
+              </option>
+            ))}
+          </Select>
+        </InputContainer>
+        <InputContainer>
+          <Label>운영시간</Label>
+          <Input
+            name="operationTime"
+            value={operationTime}
+            onChange={handleChange}
+            placeholder="오전 11:00~12:00"
+          />
+        </InputContainer>
+        <InputContainer>
+          <Label>최소 인원</Label>
+          <Input
+            name="minPerson"
+            type="number"
+            onChange={handleChange}
+            value={inputs.minPerson}
+          />
+        </InputContainer>
+        <InputContainer>
+          <Label>최대 인원</Label>
+          <Input
+            name="maxPerson"
+            type="number"
+            onChange={handleChange}
+            value={inputs.maxPerson}
+          />
+        </InputContainer>
+        <InputContainer>
+          <Label>결석 벌금</Label>
+          <Input
+            name="absentFee"
+            type="number"
+            onChange={handleChange}
+            value={inputs.absentFee}
+          />
+        </InputContainer>
+        <InputContainer>
+          <Label>지각 벌금</Label>
+          <Input
+            name="lateFee"
+            type="number"
+            onChange={handleChange}
+            value={inputs.lateFee}
+          />
+        </InputContainer>
+        <SubContainer>
+          <InputContainer>
+            <Label>상세 정보</Label>
+          </InputContainer>
+          <TextArea
+            name="info"
+            onChange={handleChange}
+            placeholder="상세 정보를 입력해주세요"
+          />
+        </SubContainer>
+        <FixedDiv>
+          <CreateBtn onClick={handleSubmit}>
+            <BtnText>스터디 생성하기</BtnText>
+          </CreateBtn>
+        </FixedDiv>
+      </Container>
+      {openModal && (
+        <>
+          <Modal>
+            <ModalTextContainer>
+              <ModalText>포인트 충전 필요</ModalText>
+              <ModalText>포인트 충전 화면으로 이동할까요?</ModalText>
+            </ModalTextContainer>
+            <ModalBtnContainer>
+              <ModalBtn onClick={toggleModal}>취소</ModalBtn>
+              <ModalBtn onClick={goPaymentPage}>충전</ModalBtn>
+            </ModalBtnContainer>
+          </Modal>
+          <BackGround onClick={toggleModal} />
+        </>
+      )}
+    </>
   );
 };
+
+const BackGround = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: ${getColor('gray')};
+  opacity: 0.7;
+  z-index: 500;
+`;
+const Modal = styled.div`
+  position: absolute;
+  width: 80%;
+  max-width: 350px;
+  background-color: ${getColor('white')};
+  z-index: 999;
+  top: 35%;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  padding: 1.5rem 0.5rem 1.5rem 0.5rem;
+`;
+
+const ModalTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-self: center;
+  width: 100%;
+  max-width: 300px;
+`;
+
+const ModalBtnContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-self: center;
+  width: 65%;
+  max-width: 200px;
+  justify-content: space-around;
+`;
+
+const ModalBtn = styled.button`
+  width: 4rem;
+  height: 2rem;
+  border: none;
+  border-radius: 20px;
+  color: ${getColor('white')};
+  background-color: ${getColor('blue')};
+`;
+
+const ModalText = styled.div`
+  margin-bottom: 1rem;
+`;
 
 const contentWidth = '65%';
 const bottomMargin = '15px';
